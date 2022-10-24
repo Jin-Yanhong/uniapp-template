384e3e3b441c2086b860d111b0562e7bea0cc374
Jin-Yanhong <820877998@qq.com>
2022-10-24 18:13:25 +0800

    update readme file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true,
+        es2021: true
     },
     extends: ['plugin:vue/vue3-essential', 'standard'],
-    overrides: [],
     parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module',
+        sourceType: 'module'
     },
-    ignores: ['.eslintrc.js'],
+    ignorePatterns: ['.eslintrc.js'],
     plugins: ['vue'],
     rules: {
-        'indent': ['warn', 4],
-        'quotes': ['warn', 'single'],
-        'semi': ['warn', 'always'],
+        indent: ['warn', 4],
+        quotes: ['warn', 'single'],
+        semi: ['warn', 'always'],
         'max-len': ['warn', { code: 360 }],
         'linebreak-style': ['warn', 'windows'],
         'object-curly-spacing': ['warn', 'always'],
         'require-jsdoc': 0,
-        'valid-jsdoc': 0,
-    },
+        'valid-jsdoc': 0
+    }
 };
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 -   本项目是基于 `vue2.0` 版本的 `uniapp模板` 项目，使用 `npm`作为包管理器
 -   接口路径位于 `/api/apiUrls.js` 下，并统一默认导出
--   全局方法在 `utils` 下
--   页面公用的 `class样式` 位于 `/style/globel` 文件下
+-   全局方法在 `/utils/index.js` 下
+-   页面公用 CSS 样式位于 `/style/globel.less` 文件下
 -   部分字段的常量在 `/static/constant.js` 下，统一维护
--   `httpBaseurl` 、`ImageBaseUrl`、 `publicKey` 在 `app.js` 下
+-   `httpBaseurl` 、`ImageBaseUrl` 在 `/app.js` 下
 -   封装的方法有
 
     -   isLogin
@@ -36,14 +36,23 @@
 
 1. 其中页面导航功能可根据业务需要添加用户登录校验，具体用法参考文档注释或源码
 
-2. 列表页面 `listHttpRequest` 具备触底加载更多，下拉刷新等基本功能，
+2. 列表页面 `listHttpRequest` 具备触底加载更多，下拉刷新功能，集体用法参照 `/pages/list` 页面
 
 ## 项目启动方式
 
-在项目根目录下执行
+1. 在项目根目录下执行
 
-```
-npm i
-```
+    ```
+    npm i
+    ```
 
-目前使用 `HBuilderX` 直接运行，后续会调整使用命令行的方式
+2. 使用 `HBuilderX` 直接运行
+    > 目前 HBuilderX ，后续会调整使用命令行的方式运行
+
+## 代码风格检查
+
+1.  在项目根目录下执行
+
+    ```
+    npm run lint
+    ```
