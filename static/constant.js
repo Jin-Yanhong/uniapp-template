a239ef64dce549623cdba34d3ccde004244e871d
Jin-Yanhong <820877998@qq.com>
2023-03-15 16:32:34 +0800

    update config fiel and delete some static file

--- static/constant.js
+++ static/constant.js
@@ -1,14 +1,3 @@
 export const genderList = [
-	{
-		label: '未知',
-		value: '0',
-	},
-	{
-		label: '男',
-		value: '1',
-	},
-	{
-		label: '女',
-		value: '2',
-	},
+	 
 ];
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
 module.exports = {
+	devServer: {
+		host: 'localhost',
+		port: 8090,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': 'api',
+				},
+			},
+		},
+	},
 	chainWebpack: config => {
 		// Config Less Loader
 		config.module
-			.rule("**/*.less")
+			.rule('**/*.less')
 			.test(/\.less$/i)
-			.use("less-loader")
-			.loader("less-loader")
+			.use('less-loader')
+			.loader('less-loader')
 			.end();
 	},
 };
